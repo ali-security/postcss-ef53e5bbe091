4e19acf2e7612463819deabf61d4704b2f19606f
Seal <info@sealsecurity.io>
2026-08-10 11:58:10 +0000

    CVE-2026-45623

--- lib/postcss.d.ts
+++ lib/postcss.d.ts
@@ -336,6 +336,11 @@ declare namespace postcss {
      * to generate correct source maps.
      */
     to?: string
+
+    /**
+     * Disable source map file protections.
+     */
+    unsafeMap?: boolean
   }
 
   export type Postcss = typeof postcss
